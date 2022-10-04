d4750eedb35775eafcb43b8c9d9ef7b996fa2619
Rajab <rkarume@gmail.com>
2022-10-04 08:08:33 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Actors Page</h1>
+    {actors.map((actor, i)=>(
+      <div key={i} >
+        <span >{actor.name}</span>
+        {actor.movies.map((movie, index)=>(
+          <ul key={index}>
+            <li  >{movie}</li>
+          </ul>
+        ))}
+      </div>
+    ))}
+  </div>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    <div>
+      {movies.map((movie, i)=>(
+        <div key={i} >
+          <span>{movie.title}</span>
+          <span>{movie.time}</span>
+          {movie.genres.map((genre, index)=>(
+            <ul key={index} >
+              <li>{genre}</li>
+            </ul>
+          ))}
+        </div>
+      ))}
+    </div>
+  </div>;
 }
 
 export default Movies;
